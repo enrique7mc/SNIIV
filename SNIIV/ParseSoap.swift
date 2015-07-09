b90f7bf39baefb4941466307b13dab2ab1935225
enrique7mc <enrique7mc@gmail.com>
2015-07-09 12:38:28 -0500

    Actualiza lectura de datos desde web service

--- SNIIV/ParseSoap.swift
+++ SNIIV/ParseSoap.swift
@@ -11,7 +11,7 @@ import SWXMLHash
 
 typealias ServiceResponse = ([ReporteGeneralPrueba]?, NSError?) -> Void
 
-class ParseSoap : NSObject, NSURLConnectionDelegate {
+class ParseSoap {
     var xmlResponse: String?
     var serviceResponse: ServiceResponse?
     
@@ -19,41 +19,35 @@ class ParseSoap : NSObject, NSURLConnectionDelegate {
         var soapMessage = "<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'> <soap:Body> <get_tot_ini xmlns='http://www.conavi.gob.mx:8080/WS_App_SNIIV' /></soap:Body></soap:Envelope>"
     
         var urlString = "http://192.168.10.166:8005/WS_App_SNIIV.asmx"
-        var url = NSURL(string: urlString)
-        var theRequest = NSMutableURLRequest(URL: url!)
-        var msgLength = String(count(soapMessage))
+        var soapAction = "http://www.conavi.gob.mx:8080/WS_App_SNIIV/get_tot_ini"
+        
+        var soapToXml = SoapToXml(message: soapMessage, url: urlString, soapAction: soapAction, method: "POST", completionHandler: handler)
+        
+        soapToXml.responseXlm()
+    }
     
-        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        theRequest.addValue(msgLength, forHTTPHeaderField: "Content-Length")
-        theRequest.addValue("http://www.conavi.gob.mx:8080/WS_App_SNIIV/get_tot_ini", forHTTPHeaderField: "SOAPAction")
-        theRequest.HTTPMethod = "POST"
-        theRequest.HTTPBody = soapMessage.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    
-        let queue:NSOperationQueue = NSOperationQueue()
-        NSURLConnection.sendAsynchronousRequest(theRequest, queue: queue, completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            
-            if let err = error {
-                println("async error: " + err.description)
-                self.serviceResponse!(nil, NSError())
-            } else {
-                if let dataString = NSString(data: data, encoding:NSUTF8StringEncoding) {
-                    self.xmlResponse = dataString as String
-                    var xml = SWXMLHash.parse(self.xmlResponse!)
-                    var sniiv = xml["soap:Envelope"]["soap:Body"]["get_tot_iniResponse"]["get_tot_iniResult"]["app_sniiv_tot"]
-                    var datos = sniiv.all.map{ elem in
-                        ReporteGeneralPrueba(cveeNT: self.getText(elem["cve_ent"]),
-                            accFinan: self.getText(elem["acc_finan"]),
-                            mtoFinan: self.getText(elem["mto_finan"]),
-                            accSubs: self.getText(elem["acc_subs"]),
-                            mtoSubs: self.getText(elem["mto_subs"]),
-                            vv: self.getText(elem["vv"]),
-                            vr: self.getText(elem["vr"]))
-                    }
-                    println(datos.count)
-                    self.serviceResponse!(datos, nil)
+    func handler(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void {
+        if let err = error {
+            println("async error: " + err.description)
+            self.serviceResponse!(nil, NSError())
+        } else {
+            if let dataString = NSString(data: data, encoding:NSUTF8StringEncoding) {
+                self.xmlResponse = dataString as String
+                var xml = SWXMLHash.parse(self.xmlResponse!)
+                var sniiv = xml["soap:Envelope"]["soap:Body"]["get_tot_iniResponse"]["get_tot_iniResult"]["app_sniiv_tot"]
+                var datos = sniiv.all.map{ elem in
+                    ReporteGeneralPrueba(cveeNT: self.getText(elem["cve_ent"]),
+                        accFinan: self.getText(elem["acc_finan"]),
+                        mtoFinan: self.getText(elem["mto_finan"]),
+                        accSubs: self.getText(elem["acc_subs"]),
+                        mtoSubs: self.getText(elem["mto_subs"]),
+                        vv: self.getText(elem["vv"]),
+                        vr: self.getText(elem["vr"]))
                 }
+                println(datos.count)
+                self.serviceResponse!(datos, nil)
             }
-        })
+        }
     }
     
     func getText(indexer: XMLIndexer) -> String {
---
+++ SNIIV/SoapToXml.swift
@@ -0,0 +1,42 @@
+//
+//  SoapToXml.swift
+//  SNIIV
+//
+//  Created by admin on 09/07/15.
+//  Copyright (c) 2015 enrique7mc. All rights reserved.
+//
+
+import Foundation
+
+class SoapToXml {
+    var message: String
+    var urlString: String
+    var soapAction: String
+    var method: String
+    var handler: (NSURLResponse!, NSData!, NSError!) -> Void
+    var xmlResponse: String?
+    
+    init(message: String, url: String, soapAction: String, method: String, completionHandler handler: (NSURLResponse!, NSData!, NSError!) -> Void) {
+        self.message = message
+        self.urlString = url
+        self.soapAction = soapAction
+        self.method = method
+        self.handler = handler
+    }
+    
+    func responseXlm() {
+        var url = NSURL(string: urlString)
+        var theRequest = NSMutableURLRequest(URL: url!)
+        var msgLength = String(count(message))
+        
+        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        theRequest.addValue(msgLength, forHTTPHeaderField: "Content-Length")
+        theRequest.addValue(soapAction, forHTTPHeaderField: "SOAPAction")
+        theRequest.HTTPMethod = method.uppercaseString
+        theRequest.HTTPBody = message.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+        
+        let queue:NSOperationQueue = NSOperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(theRequest, queue: queue, completionHandler: handler)
+    }
+}
