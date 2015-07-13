8c0fbbbd1fc9f40f9a2ce4b6fe83b516f6d23509
sap1 <sap1@conavi.gob.mx>
2015-07-13 13:53:11 -0500

    Termación Vista
    Vista terminada, pickerview (ReporteGeneral)

--- SNIIV/ReporteGeneralViewController.swift
+++ SNIIV/ReporteGeneralViewController.swift
@@ -2,13 +2,27 @@
 
 import UIKit
 
-class ReporteGeneralViewController: UIViewController {
+class ReporteGeneralViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    var opt=["Nacional","Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Coahuila", "Colima", "Chiapas" , "Chihuahua", "Distrito Federal", "Durango", "Guanajuato", "Guerrero","Hidalgo", "Jalisco",
+        "México", "Michoacán", "Morelos", "Nayarit", "Nuevo León" , "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas"]
+    
+    @IBOutlet weak var txtIndicador: UITextField!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var txtFinanMto: UILabel!
+    @IBOutlet weak var txtFinanAcc: UILabel!
+    @IBOutlet weak var txtSubAcc: UILabel!
+    @IBOutlet weak var txtSubMto: UILabel!
+    @IBOutlet weak var txtInvMto: UILabel!
+    @IBOutlet weak var txtInvAcc: UILabel!
+    var rowSelected=0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtIndicador.text=opt[rowSelected]
+        txtIndicador.enabled=false
         var parseSoap = ParseSoap()
         parseSoap.getDatosReporte() { (responseObject:[ReporteGeneralPrueba]?, error:NSError?) in
-            
             if (error) != nil {
                 println("Error obteniendo datos")
                 return
@@ -33,11 +47,39 @@ class ReporteGeneralViewController: UIViewController {
             println(entidad.vr)
             
         }
+        
+      
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+
+        return opt.count
+ 
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+       
+        return opt[row]
+        
+    }
+    
+   func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var itemSelected = opt[row]
+        txtIndicador.text = itemSelected
+    
+    //Insertar Funcion
+    
+   
+    
     }
     
 }
