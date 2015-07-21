1ca72f9a9215ba01c75de9e99e82ae6dced71826
enrique7mc <enrique7mc@gmail.com>
2015-07-21 11:40:01 -0500

    Re organiza fechas

--- SNIIV/ParseFechas.swift
+++ SNIIV/ParseFechas.swift
@@ -35,3 +35,21 @@ class ParseFechas<T>: ParseBase<Fechas> {
         }
     }
 }
+
+class Fechas {
+    var fecha_finan: String
+    var fecha_subs: String
+    var fecha_vv: String
+    
+    init() {
+        fecha_finan = ""
+        fecha_subs = ""
+        fecha_vv = ""
+    }
+    
+    init(fecha_finan: String, fecha_subs: String, fecha_vv: String) {
+        self.fecha_finan = fecha_finan
+        self.fecha_subs = fecha_subs
+        self.fecha_vv = fecha_vv
+    }
+}
